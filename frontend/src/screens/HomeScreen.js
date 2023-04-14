26b14e71d816ba5d6ef4217e4988d3aaea83c4b2
kespinak <kespinak@gmail.com>
2023-04-14 16:29:20 -0700

    updated products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,8 +8,8 @@ function HomeScreen() {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map(product => (
-          <Col sm={12} md={6} lg={4}>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4}>
             {/* <h3>{product.name}</h3> */}
             <Product product={product} />
           </Col>
