3b0dc7d969f397636815e6d1fe0f7a63ed027115
kespinak <kespinak@gmail.com>
2023-04-14 13:49:03 -0700

    added bootstrap and styled homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import './bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
